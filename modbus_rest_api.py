d627cc2a0dc2321c3fff390707722ec7557fd51f
Eder <Lukas.Eder@carissma.eu>
2025-08-17 09:50:41 +0200

    some refactoring, delay in async task loop, power delta, multiple amps power increase

--- modbus_rest_api.py
+++ modbus_rest_api.py
@@ -1,19 +1,19 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
-from pymodbus.client import ModbusTcpClient # older versions pymodbus.client.sync
 import socket
 import struct
 import asyncio
 from contextlib import asynccontextmanager
-import math
+from time import sleep
+from solar_charging import regulate_ev_charging
+from modbus_interaction import write_modbus_data, read_sma_modbus_data, read_wallbox_modbus_data, sma_devices, ev_charging_modbus_registers
+
 
 UDP_IP = "192.168.188.39"
 UDP_PORT = 9522
 
-TRIPOWER_IP = "192.168.188.45"
-SUNNY_ISLAND_IP = "192.168.188.117"
-CHARGER_ME_IP = "192.168.188.94"
-MODBUS_PORT = 502
+REGULATION_DELAY = 0.5 # Delay between loop iterations for getting some udp/modbus data and adjustion the ev charging current
+
 
 app = FastAPI()
 
@@ -24,6 +24,7 @@ battery_power = 0 # in W, negative = charging, positive = discharging
 battery_SoC = 0 # percentage, int
 ev_charging_state = 0 # 1-6
 ev_max_current = 0 # 0-16
+is_solar_only_charging = True # Boolean, if instant charging or solar charging is activated
 
 charging_states = {
     0: "Not available",
@@ -35,88 +36,12 @@ charging_states = {
     6: "F: Fault condition"
 }
 
-
-
-# values are in two registers
-def combine_registers(high, low):
-    return (high << 16) + low
-
-# Device configuration
-sma_devices = {
-    "tripower_total_power": {"ip": TRIPOWER_IP, "register": 30775, "slave": 3, "signed": False, "nan_value": 0x80000000},
-    "tripower_str1_power": {"ip": TRIPOWER_IP, "register": 30773, "slave": 3, "signed": False, "nan_value": 0x80000000},
-    "tripower_str2_power": {"ip": TRIPOWER_IP, "register": 30961, "slave": 3, "signed": False, "nan_value": 0x80000000},
-    "tripower_str3_power": {"ip": TRIPOWER_IP, "register": 30967, "slave": 3, "signed": False, "nan_value": 0x80000000},
-
-    "battery_power": {"ip": SUNNY_ISLAND_IP, "register": 30775, "slave": 3, "signed": True, "nan_value": 0x80000000},
-    "battery_SoC": {"ip": SUNNY_ISLAND_IP, "register": 30845, "slave": 3, "signed": False, "nan_value": 0xFFFFFFFF},
-}
-
-ev_charging_modbus_registers = {
-    "charging_state": {"ip": CHARGER_ME_IP, "register": 122, "slave": 1},
-    "maximum_current": {"ip": CHARGER_ME_IP, "register": 1000, "slave": 1},
-}
-
 # Allowed CORS origins hinzufügen
 origins = [
     "http://localhost:4200",  # Erlaube die Verbindung von Angular-Frontend
     "http://127.0.0.1:4200",  # Falls du von einer anderen IP aus zugreifst
 ]
 
-def write_modbus_data(ip: str, register: int, slave: int, value: int):
-    try:
-        client = ModbusTcpClient(ip, port=MODBUS_PORT, timeout=10)
-        connection = client.connect()
-        if connection:
-            response = client.write_register(register, value, slave=slave)
-        else:
-            print("Failed to connect to Modbus Server")
-        client.close()
-    except Exception as e:
-        print(f"Error writing to {ip}:{register} - {e}")
-
-def read_modbus_data(ip: str, register: int, slave: int, count: int):
-    try:
-        client = ModbusTcpClient(ip, port=MODBUS_PORT, timeout=10)
-        client.connect()
-        response = client.read_holding_registers(register, count=count, slave=slave) # older versions unit instead of slave
-        client.close()
-        if response and response.registers:
-            return response.registers
-        else:
-            return None  # Return 0 if the register is empty or there is no valid response
-    except Exception as e:
-        print(f"Error reading {ip}:{register} - {e}")
-        return None  # Return 0 in case of an exception
-
-def read_wallbox_modbus_data(ip: str, register: int, slave: int):
-    try:
-        value = read_modbus_data(ip, register, slave, 1)[0]
-        if (value != None):
-            return value
-        else:
-            return 0
-    except Exception as e:
-        print(f"Error reading {ip}:{register} - {e}")
-        return 0  # Return 0 in case of an exception
-
-# Modbus read function
-def read_sma_modbus_data(ip: str, register: int, slave: int, signed: bool, nan_value: int):
-    try:
-        registers = read_modbus_data(ip, register, slave, 2)
-        if registers:
-            value = combine_registers(registers[0], registers[1])
-            if (value == nan_value):
-                return 0
-            if signed:
-                return int.from_bytes(value.to_bytes(length=4), byteorder="big", signed=True)
-            else:
-                return value
-        else:
-            return 0  # Return 0 if the register is empty or there is no valid response
-    except Exception as e:
-        print(f"Error reading {ip}:{register} - {e}")
-        return 0  # Return 0 in case of an exception
 
 # FastAPI lifespan event to manage background tasks
 @asynccontextmanager
@@ -124,6 +49,7 @@ async def lifespan(app: FastAPI):
     udp_task = asyncio.create_task(async_task())  # Start UDP listener
     yield  # API runs while this runs in the background
     udp_task.cancel()  # Stop UDP listener when API shuts down
+
 
 app = FastAPI(lifespan=lifespan)
 
@@ -135,10 +61,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/solar-data")
 def get_power_data():
+    global grid_power, emeter_power, battery_power, battery_SoC, ev_charging_state, ev_max_current, charging_states
     data = {}
-    
+
     # Read solar power data
     data["tripower_power"] = read_sma_modbus_data(**sma_devices["tripower_total_power"])
     data["tripower_str1_power"] = read_sma_modbus_data(**sma_devices["tripower_str1_power"])
@@ -164,11 +92,11 @@ def get_power_data():
     
     return data
 
+
 # Async while true loop
 # Collects grid and emeter power information from udp messages and battery power and SoC information via modbus
 # Then starts the ev-charging regulation
 async def async_task():
-
     # UDP socket for grid_power and emeter_power data collection
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((UDP_IP, UDP_PORT))
@@ -182,11 +110,19 @@ async def async_task():
         get_grid_and_emeter_power(loop, sock)
         get_battery_power_and_soc()
 
+        if is_solar_only_charging:
+            regulate_ev_charging()
+        else: 
+            ev_max_current = read_wallbox_modbus_data(**sma_devices["maximum_current"])
+            if (ev_max_current != 16):
+                write_modbus_data(**ev_charging_modbus_registers["maximum_current"], value=16)
+        
+        sleep(REGULATION_DELAY)
+
 
 # getting grid and emeter power
 async def get_grid_and_emeter_power(loop, sock):
     global grid_power, emeter_power
-
     try:
         data, addr = await loop.sock_recvfrom(sock, 1024)  # Proper async recv
         if data[:3] == b"SMA":  # Check if the packet is from SMA
@@ -205,61 +141,12 @@ async def get_grid_and_emeter_power(loop, sock):
     except Exception as e:
         print(f"⚠️ Error in UDP server: {e}")
 
+
 def get_battery_power_and_soc():
     global battery_power, battery_SoC
-
     battery_power = read_sma_modbus_data(**sma_devices["battery_power"])
     battery_SoC = read_sma_modbus_data(**sma_devices["battery_SoC"])
 
-def regulate_ev_charging():
-    global ev_charging_state, ev_max_current
-
-    ev_charging_state = read_wallbox_modbus_data(**sma_devices["charging_state"])
-    ev_max_current = read_wallbox_modbus_data(**sma_devices["maximum_current"])
-
-    if (ev_charging_state >= 2 and ev_charging_state <= 4):
-        calculate_and_set_max_current()
-
-# calculates the maximum current for ev charging and sets a new value if needed
-# two phases charging (VW e-Golf)
-# battery should not be drained for ev charging
-# minimum current 6A - pause current 0A
-def calculate_and_set_max_current():
-    # calculate if power is drained from the grid and/or battery
-    excess_power = (grid_power + battery_power) * (-1)
-    if (ev_max_current == 0):
-        check_for_charging_start(excess_power)
-    else:
-        if excess_power > 50 and ev_max_current < 16:
-            check_for_power_increase(excess_power)
-        elif excess_power < 50:
-            check_for_power_decrease(excess_power)
-
-def check_for_charging_start(excess_power: int):
-    required_excess_power = 2800 # 2 phases, 6A + buffer
-    if (excess_power >= required_excess_power):
-        write_modbus_data(**sma_devices["maximum_current"], value=6)
-
-# check if 1A more is possible
-def check_for_power_increase(excess_power: int):
-    required_excess_power = 480 # 2 phases, 1A + buffer
-    if (excess_power >= required_excess_power):
-        current = ev_max_current + 1
-        write_modbus_data(**sma_devices["maximum_current"], value=current)
-
-# check if 1A less or charging pause is necessary
-def check_for_power_decrease(excess_power: int):
-    current_charging_power = 2*230*ev_max_current
-    one_amp_reduction_power = 2*230 # two phases, 1A
-
-    current_reduction = math.ceil((excess_power * (-1)) / one_amp_reduction_power) # round up always
-
-    new_current = ev_max_current - current_reduction
-
-    if (new_current < 6):
-        new_current = 0
-    
-    write_modbus_data(**sma_devices["maximum_current"], value=new_current)
 
 # Running:
 # uvicorn modbus_rest_api:app --host localhost --port 8000 --reload
---
+++ solar_charging.py
@@ -0,0 +1,76 @@
+import math
+from modbus_interaction import read_wallbox_modbus_data, ev_charging_modbus_registers, write_modbus_data
+from modbus_rest_api import grid_power, battery_power
+
+
+POWER_DELTA = 200 # Power in W, which should always be available as a buffer, even when the car is charging
+ONE_AMP_DELTA_POWER = 2*230 # two phases, 1A
+MIN_CHARGING_CURRENT = 6 # 6A minimum charging current
+MAX_CHARGING_CURRENT = 16 # 16A maximum charging current
+PAUSE_CHARGING_CURRENT = 0 # Charging current setting for pausing the charging process
+MIN_CHARGING_START_POWER = 2*230*MIN_CHARGING_CURRENT # two phases, minimum charging current
+
+# regulates the ev charging max current, so that no power is drained from the battery and there is always the POWER_DELTA available (e.g. for home battery charging or grid feed in)
+def regulate_ev_charging():
+    global ev_charging_state, ev_max_current
+
+    ev_charging_state = read_wallbox_modbus_data(**ev_charging_modbus_registers["charging_state"])
+    ev_max_current = read_wallbox_modbus_data(**ev_charging_modbus_registers["maximum_current"])
+
+    if (ev_charging_state >= 2 and ev_charging_state <= 4):
+        calculate_and_set_max_current()
+
+
+# calculates the maximum current for ev charging and sets a new value if needed
+# two phases charging (VW e-Golf)
+# battery should not be drained for ev charging
+# minimum current 6A - pause current 0A
+def calculate_and_set_max_current():
+    # calculate if power is drained from the grid and/or battery
+    excess_power = (grid_power + battery_power) * (-1)
+    # subtract the power delta which should always be available
+    excess_power = excess_power - POWER_DELTA
+    
+    if (ev_max_current == PAUSE_CHARGING_CURRENT):
+        check_for_charging_start(excess_power)
+    else:
+        if excess_power > 0 and ev_max_current < MAX_CHARGING_CURRENT:
+            check_for_power_increase(excess_power)
+        elif excess_power <= 0 and ev_max_current > PAUSE_CHARGING_CURRENT:
+            check_for_power_decrease(excess_power)
+
+
+# check if charging can be started
+# only run, if ev_max_current is 0 before method execution
+def check_for_charging_start(excess_power: int):
+    if (excess_power >= MIN_CHARGING_START_POWER):
+        set_charging_current(MIN_CHARGING_CURRENT)
+
+
+# check if more current is possible
+def check_for_power_increase(excess_power: int):
+    current_increase = math.floor(excess_power / ONE_AMP_DELTA_POWER) # floor always
+
+    new_current = ev_max_current + current_increase
+
+    if (new_current > MAX_CHARGING_CURRENT):
+        new_current = MAX_CHARGING_CURRENT
+    
+    set_charging_current(new_current)
+
+
+# check if less current or charging pause is necessary
+def check_for_power_decrease(excess_power: int):
+    current_reduction = math.ceil((excess_power * (-1)) / ONE_AMP_DELTA_POWER) # round up always
+
+    new_current = ev_max_current - current_reduction
+
+    if (new_current < MIN_CHARGING_CURRENT):
+        new_current = PAUSE_CHARGING_CURRENT
+    
+    set_charging_current(new_current)
+
+# only sets the new current, if it changed
+def set_charging_current(new_current):
+    if new_current != ev_max_current:
+        write_modbus_data(**ev_charging_modbus_registers["maximum_current"], value=new_current)
